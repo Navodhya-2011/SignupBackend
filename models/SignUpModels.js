6c9930ebc6a4855f51da3bdf2efc7321e20633f0
“[Navodhya <navodhya2011@gmail.com>
2022-07-07 15:36:38 +0530

    models SignUpModels.js Implementation

---
+++ models/SignUpModels.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const Student = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+
+    firstName: {
+        type: String,
+        required: true,
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+    },
+
+    dateOfBirth: {
+        type: Date,
+        default: Date.now,
+    },
+
+    mobile: {
+        type: String,
+        required: true,
+    },
+
+    status: {
+        type: Boolean,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    isStudent: {
+        type: Boolean,
+        default: true,
+    },
+})
+
+module.exports = mongoose.model('signuo', Student)
