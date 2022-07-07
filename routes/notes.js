c2191f9d1d40ce2ac8e641a1877fe984b8a109eb
“[Navodhya <navodhya2011@gmail.com>
2022-07-07 23:58:42 +0530

    routes notes.js changes

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const StudentNoteCopy = require('../models/StudentNotesModels');
 
-note.post ('/insert', (request, response) => {
+router.post ('/insert', (request, response) => {
     const StudentNote = new StudentNoteCopy({
         authId:request.body.authId,
         title:request.body.title,
@@ -19,4 +19,33 @@ note.post ('/insert', (request, response) => {
     })
 });
 
-module.exports = note
+//Get all notes
+router.get("/note", async (req, res) => {
+    try{
+        const student = await StudentNoteCopy.find();
+        res.status(200).json(student);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//Update
+router.put("/:id", async (req, res) => {
+
+    try{
+        const updateStudentNote = await  StudentNoteCopy.findByIdAndUpdate(req.params.id, {
+            authId: req.body.authId,
+            title: req.body.title,
+            text: req.body.text,
+            sender: req.body.sender,
+        });
+
+        res.status(200).json(updateStudentNote);
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router
